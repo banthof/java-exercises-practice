ff5aa9bd75f8169f915d129dfa56ba9a39731884
Anthony Flores <73008411+banthof@users.noreply.github.com>
2021-05-07 22:56:02 -0500

    Agregue cambios desde github

--- src/PruebaGit.java
+++ src/PruebaGit.java
@@ -1,6 +1,6 @@
 public class PruebaGit {
     public static void main(String[] args) {
         String nombreBus = "Rapido";
-        System.out.println("Hola");
+        System.out.println("Hola mundo desde Github");
     }
 }
